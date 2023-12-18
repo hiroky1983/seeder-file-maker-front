0adbf8e86650b84a2cd849a3dfb12840082d8c14
hiroky1983 <hirockysan1983@gmail.com>
2023-12-18 20:04:25 +0900

    コミット

--- src/components/googleads-script.tsx
+++ src/components/googleads-script.tsx
@@ -1,6 +1,6 @@
 import Script from 'next/script'
 
-const PUBLISHER_ID = 'xxxxxxxxxxxxxxxxx'
+const PUBLISHER_ID = '1869410932032409'
 
 export function GoogleAdScript() {
   if (process.env.VERCEL_ENV === 'production') {
@@ -9,7 +9,7 @@ export function GoogleAdScript() {
         async
         src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${PUBLISHER_ID}`}
         crossOrigin="anonymous"
-        // strategy="lazyOnLoad"
+        strategy="lazyOnload"
       />
     )
   }
